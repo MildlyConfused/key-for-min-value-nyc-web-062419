40acfc38db362075c18bc93119b129436eabca1d
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-19 15:28:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
     newHash[first] = value
   end
   
-  newHash
+  puts newHash
 end
 
-puts key_for_min_value({:chair => 25, :table => 85, :mattress => 450})
+key_for_min_value({:chair => 25, :table => 85, :mattress => 450})
