7c646e0817c439f20670d44a4f9ce91f36fc3f98
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-19 15:26:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,4 +7,4 @@ def key_for_min_value(name_hash)
   name_hash
 end
 
-puts key_for_min_value({:chair => 25, :table => 85, :mattress => 450}).inspect
+puts key_for_min_value({:chair => 25, :table => 85, :mattress => 450})
